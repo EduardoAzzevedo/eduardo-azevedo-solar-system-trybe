49dc4c1a41f2a5e5b1d0d44e556b74f7afb0031c
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-04-06 16:09:52 -0300

    Tenta corrigir o erro do JSON.

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 class SolarSystem extends React.Component {
   render() {
     return (
-      <section>
-        <div data-testid="solar-system"> </div>
-      </section>
+      <div data-testid="solar-system"> </div>
     );
   }
 }
