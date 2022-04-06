86a389b84e072aa89cb60e2b881f4d66ffba03c9
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-04-06 15:24:17 -0300

    Conclui requisito 2.

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <section>
+        <div data-testid="solar-system"> </div>
+      </section>
+    );
+  }
+}
+export default SolarSystem;
