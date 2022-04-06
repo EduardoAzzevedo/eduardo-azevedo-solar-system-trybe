c4308874cb033485fe77c1f0ae7456b1c1576cbb
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-04-06 16:52:09 -0300

    Conclui requisito 3.

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends React.Component {
   render() {
-    return (
-    );
+    const { headline } = this.props;
+    return (<h2>{ headline }</h2>);
   }
 }
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
+
 export default Title;
